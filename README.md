1a8085ac5e72c9c77793dabe0692a77087352d8b
Ton Machielsen--replace-all <ton@machielsen.net>
2024-07-22 17:12:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# EPAS_security
+# EDB Failover Manager demo (WIP)
+
+## Intro
+This demo is deployed using Vagrant and will deploy an EDB Postgres Advanced Server node.
+
+## Demo prep
+### Pre-requisites
+To deploy this demo the following needs to be installed in the PC from which you are going to deploy the demo:
+
+- VirtualBox (https://www.virtualbox.org/)
+- Vagrant (https://www.vagrantup.com/)
+- Vagrant Hosts plug-in (`vagrant plugin install vagrant-hosts`)
+- Vagrant Reload plug-in (`vagrant plugin install vagrant-reload`)
+- A file called `.edbtoken` with your EDB repository 2.0 token. This token can be found in your EDB account profile here: https://www.enterprisedb.com/accounts/profile
+
+The environment is deloyed in a VirtualBox private network. Adjust the IP addresses to your needs in `vars.yml`.
+
+### Provisioning VM's.
+Provision the host using `vagrant up`. This will create the bare virtual machine and will take appx. 5 minutes to complete. 
+
+After provisioning, the hosts will have the current directory mounted in their filesystem under `/vagrant`
+
+
+### Passwords
+Postgres and Enterprisedb users have passwords `postgres` and `enterprisedb`.
+
+
+## Demo cleanup
+To clean up the demo environment you just have to run `vagrant destroy`. This will remove the virtual machines and everything in it.
+
+## TODO / To fix
---
+++ bootstrap_epas.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "--- Running Bootstrap_general.sh ---"
+
+echo "--- Configuring repo ---"
+EDBTOKEN=$(cat /vagrant/.edbtoken)
+curl -1sLf 'https://downloads.enterprisedb.com/pdZe6pcnWIgmuqdR7v1L38rG6Z6wJEsY/enterprise/setup.rpm.sh' | sudo -E bash
+
+echo "--- Running updates ---"
+dnf update && dnf upgrade -y
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
+
+echo "--- Installing EPAS 16 ---"
+dnf install edb-as16-server -y
+
+sudo PGSETUP_INITDB_OPTIONS="-E UTF-8" /usr/edb/as16/bin/edb-as-16-setup initdb
+sudo mkdir -p /var/lib/edb/as16/data/conf.d
+sudo chmod 700 /var/lib/edb/as16/data/conf.d
+sudo chown -R enterprisedb:enterprisedb /var/lib/edb/as16/data/conf.d
+sudo systemctl start edb-as-16
+sudo su - enterprisedb -c "psql -c \"ALTER ROLE enterprisedb IDENTIFIED BY enterprisedb superuser;\" edb"
+sudo systemctl enable edb-as-16
+
+echo "--- Configuring pg_hba.conf ---"
+sudo systemctl stop edb-as-16
+sudo su - enterprisedb -c 'echo "
+#Replication parameters
+local   all             all                                 md5
+host    all             all             127.0.0.1/32        md5
+host    all             all             192.168.0.0/24      md5
+
+$(cat /var/lib/edb/as16/data/pg_hba.conf)" > /var/lib/edb/as16/data/pg_hba.conf'
+
+sudo systemctl restart edb-as-16
+sudo systemctl status edb-as-16
