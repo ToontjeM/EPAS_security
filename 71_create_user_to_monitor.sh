288cd899042bbf6b47f84db8a15c8b75c330daee
Ton Machielsen--replace-all <ton@machielsen.net>
2024-07-24 17:19:58 +0200

    Stuff done

--- 71_create_user_to_monitor.sh
+++ 71_create_user_to_monitor.sh
@@ -6,4 +6,4 @@ printf "${R}SELECT protect_role('webuser');${N}\n\n"
 psql -h $DBHOST -p 5444 -U enterprisedb -c "SELECT sqlprotect.protect_role('webuser');" edb
 
 printf "${R}SELECT * FROM list_protected_users;${N}\n\n"
-psql -h $DBHOST -p 5444 -U enterprisedb -c "SELECT * FROM sqlprotect.list_protected_users;" edb
+psql -h $DBHOST -p 5444 -U enterprisedb -c "SELECT * FROM sqlprotect.list_protected_users;" -x edb
